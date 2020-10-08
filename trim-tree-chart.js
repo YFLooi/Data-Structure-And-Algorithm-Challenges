b91a0488fdfdb639a58c37dc88e13cf4abda9a91
YFLooi <yihfoo@gmail.com>
2020-10-08 22:15:05 +0800

    now working on >1 leaf node. However, no branching allowed in upstream branches

--- trim-tree-chart.js
+++ trim-tree-chart.js
@@ -80,47 +80,128 @@ const newTree3 = [
   {id: 2},
   {id: 3}
 ]
+const newTree4 = [
+  {
+    id: 1,
+    children: [
+      {
+        id: 2,
+        children: [
+          {
+            id: 3,
+            children: [
+              {id: 4},
+              {id: 5}
+            ]
+          }
+        ]
+      },
+      
+    ]
+  },
+]
 
-//What this does is assemble a new tree using bits from the existing tree
-let pushCounter = 0;
-let loopCounter = 0;
 const idList = [0,1,2,3,4,5]
 const targetId = 2;
-let idMap = [];
-
-
+/**
+ * What this does is assemble a new tree using bits from the existing tree
+ * @param {*} branch 
+ * @param {*} counter 
+ * @param {*} removalTarget 
+ */
 function treeTrimmer(branch, counter, removalTarget){
   let result = [];  
+  let childrenOfChildren = [];
 
   function trimmerFunction(branch, counter, removalTarget, parentId){
     console.log(`Current iteration: ${counter}`);
 
+    //This logic stops recursion once end of input branch reached
     if(counter === branch.length){
-      //stop recursion once end of input branch reached
       return null;
-    //Dig deeper if "children" is encountered
+    //This logic handles parent nodes (The ones with "children")
     }else if("id" in branch[counter] && "children" in branch[counter]){
-      if(branch[counter]["id"] !== removalTarget){
+      //If iterating over "children" and found more "children", append them in the "children" of the 
+      //branch being processed
+      if(parentId !== null && branch[counter]["id"] !== removalTarget){
+        console.log(`Found children[] inside branch belonging to parentId ${parentId}. Making a list of them...`)
+        //newest branch would be pushed to last position in childrenOfChildren[]
+        const existingParentId = childrenOfChildren.findIndex(parent => parent["parent"] === parentId)
+
+        //Place children with the same parent together in the same child[] array
+        if(existingParentId === -1){
+          childrenOfChildren.push({
+            parent: parentId,
+            child: {id: branch[counter]["id"], children: []}
+          });  
+        } else {
+          childrenOfChildren[existingParentId]["child"].push(
+            {id: branch[counter]["id"], children: []}
+          );
+        }
+
+        //How to iterate over the contents of this "children" key to push into "children" of "children"??
+        trimmerFunction(branch[counter]["children"], 0, removalTarget, branch[counter]["id"]);   
+
+        //Reset parentId once at end of current branch
+        if(counter === branch.length){
+          parentId = null;
+        }
+      //This condition guarantees branches/leaves with id=removalTarget will not be added
+      //This condition only kicks in if not iterating over "children" of input branch
+      } else if(branch[counter]["id"] !== removalTarget){
         result.push({
           id: branch[counter]["id"],
           children: []
         });
+        console.log(`New parent branch found. result[] array becomes: ${JSON.stringify(result)}`);
+
         //Iterate over the contents of "children" key
-        //How to make sure this returns to the "children" array it worked on???
-        trimmerFunction(branch[counter]["children"], 0, removalTarget, branch[counter]["id"])   
+        trimmerFunction(branch[counter]["children"], 0, removalTarget, branch[counter]["id"]);   
       }
+    //This logic handles leaf nodes
     } else if ("id" in branch[counter]){
       if(parentId !== null && branch[counter]["id"] !== removalTarget){
-        console.log(`Child id detected`)
-        console.log(branch)
-        console.log(`Current result[]:`)
-        console.log(result)
+        //Final leaf node(s) in sequence of "children" is pushed to last position in childrenOfChildren[]
+        console.log(`Found a final leaf node inside branch belonging to parentId ${parentId}. Making a list of them...`)
+        //newest branch would be pushed to last position in childrenOfChildren[]
+        const existingParentIdLocation = childrenOfChildren.findIndex(parent => parent["parent"] === parentId)
+        console.log(`Existing parent found at childrenOfChildren[${existingParentIdLocation}]`);
 
-        //newest branch would be pushed to last position in results[]
-        result[result.length - 1]["children"].push({id: branch[counter]["id"]});
+        //Place children with the same parent together in the same child[] array
+        if(existingParentIdLocation === -1){
+          childrenOfChildren.push({
+            parent: parentId,
+            child: [{id: branch[counter]["id"]}]
+          });  
+        } else {
+          const newChild = {id: branch[counter]["id"]}  
+          childrenOfChildren[existingParentIdLocation]["child"].push(newChild)
+        }
 
-        //Reset parent id once at end of current branch
-        if(counter === branch.length){
+        console.log(`Current counter: ${counter} && current branch.length: ${branch.length}`)
+
+        //Once all leaves are accounted for, return the leaves and all subsequent branches back 
+        //to the very first parent branch
+        //This works fine with just 1 child for each parent e.g. newTree4. BUT how handle parent branches with >1 child??
+        //Clue: children from same parent branch have same parentId. Hmmm.......
+        //This version now breaks if I add a branch to an existing branch. It works fine with multiple leaves on 1 branch
+        //Reset parentId once at final leaf branch & return reverse-joined childrenOfChildren to base parent
+        if(counter === branch.length-1){
+          let joinedChildren = [];
+          
+          console.log(`childrenOfChildren found:`)
+          console.table(JSON.stringify(childrenOfChildren));
+
+          for(let i = childrenOfChildren.length-1; i>=1; --i){
+            //This inserts the last child into the "children" of the previous child
+            joinedChildren[0] = childrenOfChildren[i-1]["child"];
+            joinedChildren[0]["children"].push(childrenOfChildren[i]["child"]);
+          }
+          
+          //Insert joinedChildren[] to the parent, which is the last element in results[]
+          result[result.length-1]["children"] = joinedChildren[0];
+
           parentId = null;
         }
       //Pushes leaves into results[]
@@ -130,84 +211,14 @@ function treeTrimmer(branch, counter, removalTarget){
     }
 
     trimmerFunction(branch, counter+1, removalTarget, parentId)
-    //for(let i=0; i<branch.length; ++i){}
   }
 
   trimmerFunction(branch, counter, removalTarget, null);
+  console.log(`\nResulting childrenOfChildren[] that joined with parent: ${JSON.stringify(childrenOfChildren)}\n`);
   return result;
 }
-const objMap = treeTrimmer(inputTree, 0, "");
-console.log(JSON.stringify(objMap));
 
-let newObj = [];
-function objBuilder(outputMap){
-  console.log(`\nCurrent input: ${JSON.stringify(outputMap)}`)
-  console.log(`\n${outputMap[0].length}`)
+//currently does not remove 4,6,7,5, and 8
+const objMap = treeTrimmer(newTree4, 0, 999);
+console.log(`Output objMap: ${JSON.stringify(objMap)}`);
 
-  for(i=0; i<outputMap.length; ++i){
-    if(outputMap.length === 2){
-      const {id, children} = outputMap[i];
-      newObj = [...newObj, {["id"]:id, ["children"]:children}];
-      //objBuilder(children);
-    } else if(outputMap.length === 1){
-      const id = outputMap[i];
-      newObj = [...newObj, {["id"]:id}];
-    }
-
-  }
-
-}
-
-
-//console.log(Object.keys(newTree1[0]));
-// console.log(`loop ran ${loopCounter} times`);
-// console.log(`result.push() done ${pushCounter} times`);
-// console.log(`resulting idMap: ${JSON.stringify(idMap)}`);
-// objBuilder(idMap);
-// console.log(`newObj: ${JSON.stringify(newObj)}`);
-
-// function treeTrimmer(branch, counter){
-//   let result = [];  
-
-//   function trimmerFunction(branch, counter){
-//     for(let i=0; i<branch.length; ++i){
-//       console.log(`Current iteration: ${counter}`);
-//       if(counter === branch.length){
-//         //stop loop once end of input branch reached
-//         return null;
-//       //Dig deeper if "children" is encountered
-//       }else if(typeof value === 'object' ){
-//         console.log(`Branch to push to next level: ${JSON.stringify(value)}`);
-        
-//         for(const [key, value] of Object.entries(branch)){}
-//         let valueToCheck = value;
-//         if(value.id === targetId && "children" in value){
-//           delete valueToCheck["id"]; 
-//           delete valueToCheck["children"]; 
-//         //Must put this condition here, otherwise "children" will be missed
-//         } else if(value.id === targetId){
-//           delete valueToCheck["id"];
-//         }
-
-//         result.push(treeTrimmer(valueToCheck));
-//       } else {
-//         console.log(`Current content in result[]: ${JSON.stringify(result)}`);
-//         console.log(`Branch to push to results[]: ${JSON.stringify(branch)}`);
-//         pushCounter += 1; //= number of leaf nodes
-//         result.push(value);
-
-//         //check if "children" are present
-//         // if("children" in branch){
-//         //   result.push({["id"]:value});
-//         // } else {
-//         //   //result.push({["children"]:[{[key]:value}]});
-//         //   result.push({["id"]:value});
-//         // }
-        
-//       }
-//     }
-//   }
-
-//   trimmerFunction(branch, counter);
-//   return result;
-// }
